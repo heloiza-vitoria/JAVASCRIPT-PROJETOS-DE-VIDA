aeb30edc801e3e115622a1fab3c82d3ca5058b3a
heloiza-vitoria <heloiza.farias.rodrigues@escola.pr.gov.br>
2026-06-24 13:59:04 -0300

    alteraçao 1

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    botoes[i]
-    console.log( botoes[i]);
+    botoes[i].onclick = function(){
+
+    }
+    console.log();
 }
